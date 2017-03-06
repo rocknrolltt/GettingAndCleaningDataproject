b3a84fb081af6b7b83b4b7457356bf97ad29d306
Ting Wu <ting.wu@verisk.com>
2017-03-06 16:55:27 -0500

    Getting-and-Cleaning-Data-Course-Project

---
+++ run_analysis.r
@@ -0,0 +1,5 @@
+require(knitr)
+require(markdown)
+setwd("C:\\ting\\coursea\\getting_cleaning_data\\project\\GettingAndCleaningData-master\\GettingAndCleaningData-master\\Project")
+knit("run_analysis.Rmd", encoding="ISO8859-1")
+markdownToHTML("run_analysis.md", "run_analysis.html")
